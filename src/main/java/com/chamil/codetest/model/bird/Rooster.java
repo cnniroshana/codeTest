9bba456271849cc0f4c469267ad3680767aef5d8
cnniroshana <chamil.niroshana@gmail.co>
2020-02-03 00:57:22 +0800

    Q3. rooster class. cannot represent without inheritance

---
+++ src/main/java/com/chamil/codetest/model/bird/Rooster.java
@@ -0,0 +1,8 @@
+package com.chamil.codetest.model.bird;
+
+public class Rooster extends Chicken {
+    @Override
+    public void makeSound() {
+        System.out.println("Cock-a-doodle-doo");
+    }
+}
